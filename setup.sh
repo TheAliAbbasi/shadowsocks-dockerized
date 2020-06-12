907bca0d6abd8b1ea1f5cb64b7d707d957581d9e
TheAli <thealiabbasi@gmail.com>
2020-06-13 01:12:10 +0430

    configured v2ray

--- setup.sh
+++ setup.sh
@@ -16,8 +16,12 @@ echo 'connection string is: '$connection_string
 
 qrencode -t ANSIUTF8 $connection_string
 
-# Issue tls certificate
-acme.sh --issue --dns dns_cf --debug -d $DOMAIN
+# Issue tls certificate (if not exists)
+if [ ! -f "/conf/ssl.pem" ]; then
+    acme.sh --issue --dns dns_cf -d $DOMAIN --cert-file /conf/ssl.pem --key-file /conf/ssl.key --ca-file /conf/ssl.pem
+fi
+
+SS_CONFIG=$SS_CONFIG" --plugin v2ray-plugin --plugin-opts 'server;tls;host=$DOMAIN;cert=/conf/ssl.pem;key=/conf/ssl.key;mode:tcp_only;fast-open;localAddr=127.0.0.1;localPort=1080;remoteAddr=0.0.0.0;remotePort=443"
 
 /entrypoint.sh
 # ss-server -c /conf/config.json
