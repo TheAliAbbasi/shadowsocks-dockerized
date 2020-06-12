e8c4507d0f27d14c681fb956950d90e27050c329
TheAli <thealiabbasi@gmail.com>
2020-06-12 11:06:12 +0430

    WIP: implementing v2ray

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,15 @@
 FROM mritd/shadowsocks
 
+ENV SS_CONFIG -c /conf/config.json
+ENV SS_MODULE ss-server
+ENV PATH $PATH:/root/.acme.sh
+
 SHELL [ "/bin/sh", "-c" ]
 
-RUN apk add --no-cache jq pwgen openssl libqrencode
+RUN apk add --no-cache jq pwgen openssl libqrencode sed
+
+# Install acme.sh
+RUN wget https://get.acme.sh -O - -q | sh
 
 COPY setup.sh /setup.sh
 COPY config.json /conf/config.json
--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 #Set a random string password
 password=$(pwgen -n 20 -1)
-sed -i 's/""/"'$password'"/g' /conf/config.json
+sed -i 's/"<YOUR_PASSWORD>"/"'$password'"/g' /conf/config.json
 
 method=$(jq -r ". .method" /conf/config.json)
 password=$(jq -r ". .password" /conf/config.json)
@@ -16,4 +16,8 @@ echo 'connection string is: '$connection_string
 
 qrencode -t ANSIUTF8 $connection_string
 
-ss-server -c /conf/config.json
+# Issue tls certificate
+acme.sh --issue --dns dns_cf --debug -d $DOMAIN
+
+/entrypoint.sh
+# ss-server -c /conf/config.json
